e14eaa9e3506d341d0d7e3710f32caff70fcf1a5
chris <christopher.hasenberg@hotmail.de>
2017-04-16 23:12:29 +0200

    setup works

--- src/owncloud/setup.sh
+++ src/owncloud/setup.sh
@@ -1,3 +1,4 @@
+/etc/init.d/apache2 start
 echo -n "Enter your server name > "
 read text
 echo "ServerName $text" >> /etc/apache2/apache2.conf
@@ -16,12 +17,20 @@ if [ $? -eq 0 ]
 then
 	echo "Connection to $text is successfully established."
 fi
+#Install MySql
+echo "Generate MySql password"
+MYSQL_PASS=`pwgen -s 40 1`
+export DEBIAN_FRONTEND=noninteractive
+echo "mysql-server-5.6 mysql-server/root_password password $MYSQL_PASS" |  debconf-set-selections
+echo "mysql-server-5.6 mysql-server/root_password_again password $MYSQL_PASS" |  debconf-set-selections
+apt-get -y install mysql-server
+mysqladmin -u root $MYSQL_PASS
 #install php
 apt-get -y install php libapache2-mod-php php-mcrypt php-mysql
 #move php file to the first position within the string
 sed -i -e 's/DirectoryIndex index.html index.cgi index.pl index.php index.xhtml index.htm/DirectoryIndex index.php index.html index.cgi index.pl index.xhtml index.htm/g'  /etc/apache2/mods-enabled/dir.conf
 #Restart Apache
-/etc/init.d/apache2 restart 
+/etc/init.d/apache2 restart
 #Download OwnCloud installation key
 curl https://download.owncloud.org/download/repositories/stable/Ubuntu_16.04/Release.key | apt-key add -
 #add file to apt sources
@@ -43,5 +52,7 @@ MYSQL_ROOT="root"
 mysql -u root -p$MYSQL_ROOT -e "CREATE DATABASE owncloud; GRANT ALL ON owncloud.* to 'owncloud'@'localhost' IDENTIFIED BY '$PASS';"
 touch info.txt
 echo "MySQL user created.  " >> info.txt
+echo "MySQL root pwd: $MYSQL_PASS" >> info.txt
+echo "Create Owncloud user:"
 echo "Username:   owncloud" >> info.txt
 echo "Password:   $PASS" >> info.txt
